ea68c49e50ee48f36cc900469a5e436e82a563a6
Stephen Llewellyn <gandalf1_barrysworld@yahoo.co.uk>
2018-01-28 18:17:41 +0000

    Fix to server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+
+var distFolder = __dirname + '/../dist';
+
+console.log(`distFolder: ${distFolder}`);
+
+app.use(express.static(distFolder));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 3000);
