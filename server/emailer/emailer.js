adabfd88192f12abba19130c6a0ffd91eaf20f06
SteveL <SteveL@users.noreply.github.com>
2018-01-29 14:02:05 +0000

    Use env variables for credentials

--- server/emailer/emailer.js
+++ server/emailer/emailer.js
@@ -1,28 +1,28 @@
 var nodemailer = require('nodemailer');
 //var config = require("./../../config/secrets");
 
-console.log('Running emailer svc');
+//console.log('Running emailer svc');
 
 var transporter = nodemailer.createTransport({
     service: 'SendGrid',
     auth: {
-      user: 'config.sendGridUser',
-      pass: 'config.sendGridPassword'
+      user: process.env.tck_sendGridUser,
+      pass: process.env.tck_sendGridPassword
     },
     tls: { rejectUnauthorized: false }
   });
 
-  console.log('Created transport');
+  //console.log('Created transport');
   
   var dummyMailOptions = {
-    from: 'config.gmailUser',
-    to: 'config.yahooUser',
+    from: process.env.tck_gmailUser,
+    to: process.env.tck_YahooUser,
     subject: 'Sending Email using Node.js',
     text: 'That was easy!'
   };
 
-  console.log('Set up mail options');
-
+  //console.log('Set up mail options: ', JSON.stringify(dummyMailOptions));
+/*
   var sendMail = function (mailOptions) {
     transporter.sendMail(mailOptions, function(error, info){
         if (error) {
@@ -32,7 +32,29 @@ var transporter = nodemailer.createTransport({
         }
       });
   };
-  
-  sendMail(dummyMailOptions);
+*/
+  var sendMail = function (emailRequest) {
 
-  console.log('Finished');
+    var mailOptions = {
+      from: process.env.tck_gmailUser,
+      to: process.env.tck_YahooUser,
+      subject: 'Email request from TCK Website',
+      text: `Details: First Name: ${emailRequest.firstName}, Last Name: ${emailRequest.lastName}`
+    };
+
+    console.log('Sending email with text : ', mailOptions.text);
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+  };
+  
+  //sendMail(dummyMailOptions);
+
+  //console.log('Finished');
+
+  module.exports = { sendMail };
--- server/server.js
+++ server/server.js
@@ -1,14 +1,52 @@
 // server.js
 const express = require('express');
+const bodyParser = require('body-parser');
+const emailer = require('./emailer/emailer');
+
 const app = express();
+
 // Run the app by serving the static files
 // in the dist directory
 
 var distFolder = __dirname + '/../dist';
 
-console.log(`distFolder: ${distFolder}`);
+//console.log(`distFolder: ${distFolder}`);
 
 app.use(express.static(distFolder));
+app.use(bodyParser.json());
+
+app.post('/email', (req, res) => {
+
+    //console.log('req.body: ', JSON.stringify(req.body));
+
+    var firstName = req.body.firstName;
+
+    //console.log('Extracted FirstName');
+
+    var emailRequest = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    };
+
+    //console.log('Setup email request');
+
+    emailer.sendMail(emailRequest);
+
+    res.send({
+        msg: "Email request sent"
+    });
+
+    /*
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+    */
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 3000);
+
+console.log('Server listening on port', process.env.PORT || 3000);
